331d64c2ccde99fa3ba044e0423113068977a7a0
jonathan <jnthnkabongo@gmail.com>
2024-03-05 23:56:47 +0100

    le projet brut

---
+++ gestionapp/resources/views/administration/pages/transaction/modifier.blade.php
@@ -0,0 +1,55 @@
+@extends('administration.layouts-admin.entete-admin')
+@section('content')
+    <main>
+        <div class="container-fluid px-4">
+            <h1 class="mt-4">Transactions</h1>
+            <ol class="breadcrumb mb-4">
+                <li class="breadcrumb-item active">Modifier Transaction</li>
+            </ol>
+            <hr class="mb-4">
+            <div class="row">
+                <div class="col-lg-1 col-md-1"></div>
+                <div class="col-lg-10 col-md-10">
+                    <div class="card">
+                        <div class="card-header pt-4"></div>
+                        <div class="card-body">
+                            <form class="row g-3">
+                                <div class="col-md-6">
+                                <label for="inputEmail4" class="form-label">Numéro Réçu</label>
+                                <input type="email" class="form-control" id="inputEmail4">
+                                </div>
+                                <div class="col-md-6">
+                                    <label for="inputEmail4" class="form-label">Nom</label>
+                                    <input type="email" class="form-control" id="inputEmail4">
+                                </div>
+                                <div class="col-md-6">
+                                    <label for="inputEmail4" class="form-label">Matricule</label>
+                                    <input type="email" class="form-control" id="inputEmail4">
+                                </div>
+                                <div class="col-md-6">
+                                    <label for="inputEmail4" class="form-label">Montant</label>
+                                    <input type="email" class="form-control" id="inputEmail4">
+                                </div>
+                                <div class="col-md-6">
+                                <label for="inputPassword4" class="form-label">Date du jour</label>
+                                <input type="password" class="form-control" id="inputPassword4">
+                                </div>
+                                <div class="col-6">
+                                <label for="inputAddress" class="form-label">Motif</label>
+                                <input type="text" class="form-control" id="inputAddress" >
+                                </div>
+                                <div class="col-12">
+                                <button type="submit" class="btn btn-primary w-100">Enregistrer</button>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="card-footer text-muted  pt-4"></div>
+                    </div>
+
+                </div>
+                <div></div>
+            </div>
+
+        </div>
+    </main>
+@endsection
---
+++ gestionapp/resources/views/auth/auth.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Absolut Group</title>
+        <link href="css/styles.css" rel="stylesheet" />
+        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+    </head>
+    <body class="bg-light">
+        <div id="layoutAuthentication">
+            <div id="layoutAuthentication_content">
+                <main>
+                    <div class="container">
+                        <div class="row justify-content-center mt-5">
+                            <div class="col-lg-5 mt-5">
+                                <div class="card shadow-lg border-0 rounded-lg mt-5">
+                                    <div class="card-header "><h3 class="text-center font-weight-light my-4">Connexion</h3></div>
+                                    <div class="card-body">
+                                        <form>
+                                            <div class="form-floating mb-3">
+                                                <input class="form-control" id="inputEmail" type="email" placeholder="name@example.com" />
+                                                <label for="inputEmail">Adresse E-mail</label>
+                                            </div>
+                                            <div class="form-floating mb-3">
+                                                <input class="form-control" id="inputPassword" type="password" placeholder="Password" />
+                                                <label for="inputPassword">Mot de passe</label>
+                                            </div>
+                                            <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
+                                                <a class="small" href="password.html">Mot de passe oublier?</a>
+                                                <a class="btn btn-primary" href="{{ route('index') }}">Se connecter</a>
+                                            </div>
+                                        </form>
+                                    </div>
+                                    <div class="card-footer bg-light text-center pt-5 py-3">
+                                        <div></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            </div>
+            <div id="layoutAuthentication_footer">
+                <footer class="py-4 bg-dark mt-auto">
+                    <div class="container-fluid px-4">
+                        <div class="d-flex align-items-center justify-content-between small">
+                            <div class="text-muted">Copyright &copy; Bestech Consult 2024</div>
+                            <div>
+                                <a href="#">Termes &amp; Conditions</a>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
